09bfcb70e64fc50241fcedbebf775d91c18b7f76
Roman <reclosedev@gmail.com>
2012-04-11 21:21:11 +0400

    connection option for MongoCache

--- requests_cache/backends/mongo.py
+++ requests_cache/backends/mongo.py
@@ -13,11 +13,13 @@ from requests_cache.backends.mongodict import MongoDict, MongoPickleDict
 class MongoCache(BaseCache):
     """ ``mongo`` cache backend.
     """
-    def __init__(self, db_name='requests-cache'):
+    def __init__(self, db_name='requests-cache', **options):
         """
         :param db_name: database name (default: ``'requests-cache'``)
+        :param connection: (optional) ``pymongo.Connection``
         """
         super(MongoCache, self).__init__()
-        self.responses = MongoPickleDict(db_name, 'responses')
+        self.responses = MongoPickleDict(db_name, 'responses',
+                                         options.get('connection'))
         self.url_map = MongoDict(db_name, 'urls', self.responses.connection)
 
--- requests_cache/backends/sqlite.py
+++ requests_cache/backends/sqlite.py
@@ -16,11 +16,11 @@ class DbCache(BaseCache):
     Reading is fast, saving is a bit slower. It can store big amount of data
     with low memory usage.
     """
-    def __init__(self, location='cache', *args, **kwargs):
+    def __init__(self, location='cache', **options):
         """
         :param location: database filename prefix (default: ``'cache'``)
         """
-        super(DbCache, self).__init__(*args, **kwargs)
+        super(DbCache, self).__init__()
         self.responses = DbPickleDict(location, 'responses')
         self.url_map = DbDict(location, 'urls', self.responses)
 
