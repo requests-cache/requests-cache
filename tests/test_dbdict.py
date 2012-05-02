bc373d5f3f977b2ec397fb725209c29847911bd1
Roman Haritonov <reclosedev@gmail.com>
2012-05-02 12:03:15 +0400

    extract constant db_name in test

--- tests/test_dbdict.py
+++ tests/test_dbdict.py
@@ -7,11 +7,14 @@ sys.path.insert(0, os.path.abspath('..'))
 import unittest
 from requests_cache.backends.dbdict import DbDict, DbPickleDict
 
+DB_NAME = 'test'
+
+
 class DbdictTestCase(unittest.TestCase):
     def test_reusable_dicts(self):
-        d1 = DbDict('test', 'table1')
-        d2 = DbDict('test', 'table2', d1)
-        d3 = DbDict('test', 'table3', d1)
+        d1 = DbDict(DB_NAME, 'table1')
+        d2 = DbDict(DB_NAME, 'table2', d1)
+        d3 = DbDict(DB_NAME, 'table3', d1)
         d1[1] = 1
         d2[2] = 2
         d3[3] = 3
@@ -20,10 +23,10 @@ class DbdictTestCase(unittest.TestCase):
         self.assertEqual(list(d3.keys()), [3])
 
         with self.assertRaises(ValueError):
-            d4 = DbDict('test', 'table1', d1)
+            d4 = DbDict(DB_NAME, 'table1', d1)
 
     def test_bulk_commit(self):
-        d = DbDict('test', 'test')
+        d = DbDict(DB_NAME, 'table')
         d.clear()
         with d.bulk_commit():
             for i in range(100):
@@ -31,29 +34,29 @@ class DbdictTestCase(unittest.TestCase):
         self.assertEqual(list(d.keys()), list(range(100)))
 
     def test_switch_commit(self):
-        d = DbDict('test')
+        d = DbDict(DB_NAME)
         d.clear()
         d[1] = 1
-        d = DbDict('test')
+        d = DbDict(DB_NAME)
         self.assertIn(1, d)
 
         d.can_commit = False
         d[2] = 2
         self.assertEqual(d[2], 2)
 
-        d = DbDict('test')
+        d = DbDict(DB_NAME)
         self.assertNotIn(2, d)
         self.assert_(d.can_commit)
 
     def test_str(self):
-        d = DbDict('test')
+        d = DbDict(DB_NAME)
         d.clear()
         d[1] = 1
         d[2] = 2
         self.assertEqual(str(d), '{1: 1, 2: 2}')
 
     def test_del(self):
-        d = DbDict('test')
+        d = DbDict(DB_NAME)
         d.clear()
         for i in range(5):
             d[i] = i
@@ -66,15 +69,15 @@ class DbdictTestCase(unittest.TestCase):
             del d[0]
 
     def test_picklable_dict(self):
-        d = DbPickleDict('test')
+        d = DbPickleDict(DB_NAME)
         d[1] = ForPickle()
-        d = DbPickleDict('test')
+        d = DbPickleDict(DB_NAME)
         self.assertEqual(d[1].a, 1)
         self.assertEqual(d[1].b, 2)
 
     def test_len(self):
         n = 5
-        d = DbDict('test')
+        d = DbDict(DB_NAME)
         for i in range(n):
             d[i] = i
         self.assertEqual(len(d), 5)
