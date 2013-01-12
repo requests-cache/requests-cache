72c69c6e46e4d51c2d11d8225712e2071551b3dc
Roman Haritonov <reclosedev@gmail.com>
2013-01-12 19:13:04 +0400

    additional tests for monkey-patching

--- tests/test_monkey_patch.py
+++ tests/test_monkey_patch.py
@@ -5,17 +5,11 @@ import os, sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import unittest
-import time
-import json
-from collections import defaultdict
 
-from requests import Session
 import requests
-from requests import Request
 
 import requests_cache
 from requests_cache import CachedSession
-from requests_cache.compat import bytes, str, is_py3
 
 CACHE_NAME = 'requests_cache_test'
 CACHE_BACKEND = 'sqlite'
@@ -23,18 +17,28 @@ FAST_SAVE = False
 
 
 class MonkeyPatchTestCase(unittest.TestCase):
+    def setUp(self):
+        requests_cache.install_cache(name=CACHE_NAME, backend=CACHE_BACKEND)
+        requests.Session().cache.clear()
+        requests_cache.uninstall_cache()
 
     def test_install_uninstall(self):
         for _ in range(2):
             requests_cache.install_cache(name=CACHE_NAME, backend=CACHE_BACKEND)
-            self.assert_(isinstance(requests.Session(), CachedSession))
-            self.assert_(isinstance(requests.sessions.Session(), CachedSession))
-            self.assert_(isinstance(requests.session(), CachedSession))
+            self.assertTrue(isinstance(requests.Session(), CachedSession))
+            self.assertTrue(isinstance(requests.sessions.Session(), CachedSession))
+            self.assertTrue(isinstance(requests.session(), CachedSession))
             requests_cache.uninstall_cache()
-            self.assert_(not isinstance(requests.Session(), CachedSession))
-            self.assert_(not isinstance(requests.sessions.Session(), CachedSession))
-            self.assert_(not isinstance(requests.session(), CachedSession))
+            self.assertFalse(isinstance(requests.Session(), CachedSession))
+            self.assertFalse(isinstance(requests.sessions.Session(), CachedSession))
+            self.assertFalse(isinstance(requests.session(), CachedSession))
 
+    def test_requests_from_cache(self):
+        requests_cache.install_cache(name=CACHE_NAME, backend=CACHE_BACKEND)
+        r = requests.get('http://httpbin.org/get')
+        self.assertFalse(r.from_cache)
+        r = requests.get('http://httpbin.org/get')
+        self.assertTrue(r.from_cache)
 
 
 if __name__ == '__main__':
