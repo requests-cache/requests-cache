6fc1b3e3a0500d66b3df0ab77863b37302f2c228
Roman Haritonov <reclosedev@gmail.com>
2013-01-12 18:24:15 +0400

    fix thread_safety test

--- tests/test_thread_safety.py
+++ tests/test_thread_safety.py
@@ -8,7 +8,7 @@ from threading import Thread
 import unittest
 
 import requests
-import requests_cache
+from requests_cache import CachedSession
 
 CACHE_NAME = 'requests_cache_test'
 
@@ -18,13 +18,13 @@ class ThreadSafetyTestCase(unittest.TestCase):
     def test_caching_with_threads(self):
 
         def do_tests_for(backend):
-            requests_cache.configure(CACHE_NAME, backend)
-            requests_cache.clear()
+            s = CachedSession(CACHE_NAME, backend)
+            s.cache.clear()
             n_threads = 10
             url = 'http://httpbin.org/get'
             def do_requests(url, params):
                 for i in range(10):  # for testing write and read from cache
-                    requests.get(url, params=params)
+                    s.get(url, params=params)
 
             for _ in range(20): # stress test
                 threads = [Thread(target=do_requests, args=(url, {'param': i})) for i in range(n_threads)]
@@ -34,7 +34,7 @@ class ThreadSafetyTestCase(unittest.TestCase):
                     t.join()
 
                 for i in range(n_threads):
-                    self.assert_(requests_cache.has_url('%s?param=%s' % (url, i)))
+                    self.assert_(s.cache.has_url('%s?param=%s' % (url, i)))
 
         for backend in ('sqlite', 'mongodb'):
             try:
