92bac0cb28b9949a2281884ad1b24f9c0cd435ca
Roman Haritonov <reclosedev@gmail.com>
2013-04-18 19:34:18 +0400

    Mock redis for readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['pymongo']
+MOCK_MODULES = ['pymongo', 'redis']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
