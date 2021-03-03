c25514312bafb89fe86663c0112d478714d1f83e
Jordan Cook <jordan.cook@pioneer.com>
2021-03-03 09:51:15 -0600

    Documentation build improvments:
    * Add `.readthedocs.yml` build config
    * Remove mocks for backend modules (pymongo, redis-py, etc.), and make Readthedocs install them before building
    * Add Sphinx extensions for:
        * Google-style docstrings
        * 'Copy' button for code snippets
        * Automatic type annotation docs
        * Links to view source code
    * Add documentation requirements to setup.py for building docs locally
    * Remove config for unused Sphinx outputs

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@
 from itertools import chain
 from setuptools import find_packages, setup
 
+from requests_cache import __version__
+
 extras_require = {
     # Packages used for CI jobs
     'build': ['coveralls', 'twine', 'wheel'],
     # Packages for all supported backends
     'backends': ['boto3', 'pymongo', 'redis'],
+    # Packages used for documentation builds
+    'docs': [
+        'm2r2',
+        'Sphinx~=3.5.1',
+        'sphinx-autodoc-typehints',
+        'sphinx-copybutton',
+        'sphinx-rtd-theme',
+        'sphinxcontrib-apidoc',
+    ],
     # Packages used for testing both locally and in CI jobs
     'test': [
         'black==20.8b1',
@@ -23,7 +34,7 @@ extras_require['dev'] = list(chain.from_iterable(extras_require.values()))
 setup(
     name='requests-cache',
     packages=find_packages(),
-    version='0.6.0',
+    version=__version__,
     author='Roman Haritonov',
     author_email='reclosedev@gmail.com',
     url='https://github.com/reclosedev/requests-cache',
