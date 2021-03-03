e830db17976899f4a5a70edbdb9efe85c4909af6
Jordan Cook <jordan.cook@pioneer.com>
2021-03-03 07:47:53 -0600

    Add automated pypi deployments, and deploy a pre-release build (only installable with pip install --pre

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from itertools import chain
 from setuptools import find_packages, setup
 
 extras_require = {
+    # Packages used for CI jobs
+    'build': ['coveralls', 'twine', 'wheel'],
     # Packages for all supported backends
     'backends': ['boto3', 'pymongo', 'redis'],
     # Packages used for testing both locally and in CI jobs
     'test': [
         'black==20.8b1',
-        'coveralls',
         'flake8',
         'isort',
         'pre-commit',
@@ -22,7 +23,7 @@ extras_require['dev'] = list(chain.from_iterable(extras_require.values()))
 setup(
     name='requests-cache',
     packages=find_packages(),
-    version='0.5.2',
+    version='0.6.0',
     author='Roman Haritonov',
     author_email='reclosedev@gmail.com',
     url='https://github.com/reclosedev/requests-cache',
