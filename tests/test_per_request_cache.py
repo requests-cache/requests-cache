6b8b308a1d7081e550b102982e8b618bbe7c8ed9
Sebastian Höffner <info@sebastian-hoeffner.de>
2021-03-05 12:40:25 +0100

    Replace assert statements with methods

--- tests/test_per_request_cache.py
+++ tests/test_per_request_cache.py
@@ -20,67 +20,67 @@ class PerRequestCachedSessionTest(unittest.TestCase):
 
     def test_default_cache_always(self):
         response = requests.get(self.url)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(self.url)
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
         response = requests.get(self.url, expire_after='default')
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
     def test_default_cache_never(self):
         requests_cache.install_cache(backend='memory', expire_after=-1)
 
         response = requests.get(self.url)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(self.url)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(self.url, expire_after='default')
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
     def test_positive_cache(self):
         response = requests.get(self.url, expire_after=0.1)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         time.sleep(0.5)
 
         response = requests.get(self.url)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         # This should delete the cached entry before as it changed
         response = requests.get(self.url, expire_after=5)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         # This should not delete the cached entry before as it didn't change
         response = requests.get(self.url, expire_after=5)
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
     def test_negative_cache(self):
         response = requests.get(self.url)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(self.url)
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
         response = requests.get(self.url, expire_after=-1)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(self.url, expire_after=-1)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
     def test_cache_invalidation(self):
-        assert not requests.get(self.url, expire_after=1).from_cache
-        assert requests.get(self.url).from_cache
+        self.assertFalse(requests.get(self.url, expire_after=1).from_cache)
+        self.assertTrue(requests.get(self.url).from_cache)
         time.sleep(1.2)
-        assert not requests.get(self.url).from_cache
+        self.assertFalse(requests.get(self.url).from_cache)
 
-        assert not requests.get(self.url, expire_after=-1).from_cache
-        assert not requests.get(self.url).from_cache
+        self.assertFalse(requests.get(self.url, expire_after=-1).from_cache)
+        self.assertFalse(requests.get(self.url).from_cache)
 
-        assert not requests.get(self.url, expire_after=1).from_cache
-        assert requests.get(self.url).from_cache
+        self.assertFalse(requests.get(self.url, expire_after=1).from_cache)
+        self.assertTrue(requests.get(self.url).from_cache)
 
     def test_auto_clear_expired(self):
         requests_cache.install_cache(backend='memory', expire_after=1)
@@ -88,80 +88,80 @@ class PerRequestCachedSessionTest(unittest.TestCase):
         second_url = HTTPBIN_URL + 'anything'
 
         response = requests.get(self.url, expire_after=5)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(self.url)
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
         response = requests.get(second_url)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         time.sleep(2)
 
         response = requests.get(self.url)
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
         response = requests.get(second_url, expire_after=10)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(second_url)
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
     def test_remove_expired(self):
         response = requests.get(self.url)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(self.url)
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
         second_url = HTTPBIN_URL + 'anything'
 
         response = requests.get(second_url, expire_after=2)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(second_url)
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
         third_url = HTTPBIN_URL
 
         response = requests.get(third_url, expire_after=10)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(third_url)
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
-        assert len(requests.Session().cache.responses) == 3
+        self.assertEqual(len(requests.Session().cache.responses), 3)
 
         time.sleep(2)
 
         requests_cache.remove_expired_responses()
 
-        assert len(requests.Session().cache.responses) == 2
+        self.assertEqual(len(requests.Session().cache.responses), 2)
 
     def test_remove_expired_expire_by_default(self):
         requests_cache.install_cache(backend='memory', expire_after=1)
 
         response = requests.get(self.url)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(self.url)
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
         second_url = HTTPBIN_URL + 'anything'
 
         response = requests.get(second_url, expire_after=10)
-        assert not response.from_cache
+        self.assertFalse(response.from_cache)
 
         response = requests.get(second_url)
-        assert response.from_cache
+        self.assertTrue(response.from_cache)
 
-        assert len(requests.Session().cache.responses) == 2
+        self.assertEqual(len(requests.Session().cache.responses), 2)
 
         time.sleep(1)
 
         requests_cache.core.remove_expired_responses()
 
-        assert len(requests.Session().cache.responses) == 1
+        self.assertEqual(len(requests.Session().cache.responses), 1)
 
 
 class ContextManagerTest(unittest.TestCase):
@@ -172,20 +172,20 @@ class ContextManagerTest(unittest.TestCase):
         url = HTTPBIN_URL + 'delay/2'
         with requests_cache.enabled('test_cache', expire_after=10):
             response = requests.get(url)
-            assert not response.from_cache
+            self.assertFalse(response.from_cache)
 
             response = requests.get(url)
-            assert response.from_cache
+            self.assertTrue(response.from_cache)
 
         start = time.time()
         response = requests.get(url)
         end = time.time()
-        assert not hasattr(response, 'from_cache')
-        assert end - start >= 1.5
+        self.assertFalse(hasattr(response, 'from_cache'))
+        self.assertGreaterEqual(end - start, 1.5)
 
         with requests_cache.enabled('test_cache'):
             response = requests.get(url)
-            assert response.from_cache
+            self.assertTrue(response.from_cache)
 
 
 if __name__ == '__main__':
