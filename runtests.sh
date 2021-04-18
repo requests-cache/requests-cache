1fcbe2b30e4fdd96c345b5ea488ae51d3f542273
Jordan Cook <jordan.cook@pioneer.com>
2021-04-18 14:42:49 -0500

    Run DynamoDB integration tests last, since the DynamoDB container takes longer to initialize

--- runtests.sh
+++ runtests.sh
@@ -5,4 +5,6 @@ COVERAGE_ARGS='--cov --cov-report=term --cov-report=html'
 
 # Run unit tests first (and with multiprocessing) to fail quickly if there are issues
 pytest -s tests/unit --numprocesses=auto $COVERAGE_ARGS
-pytest -s tests/integration --cov-append $COVERAGE_ARGS
+pytest -s -k 'not dynamodb' tests/integration --cov-append $COVERAGE_ARGS
+# Run DynamoDB tests last, since that container takes the longest to initialize
+pytest -s tests/integration/test_dynamodb.py --cov-append $COVERAGE_ARGS
