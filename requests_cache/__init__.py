ec6d2fb6bb082e95bbf452fafe7397d9bd99d9b2
Jordan Cook <jordan.cook@pioneer.com>
2021-04-29 15:41:46 -0500

    Build pre-releases from 'pre-release' branch, and set version suffix in CI config

--- requests_cache/__init__.py
+++ requests_cache/__init__.py
@@ -3,6 +3,7 @@ from logging import getLogger
 from os import getenv
 
 __version__ = '0.7.0'
+__version__ += getenv('PRE_RELEASE_SUFFIX', '')
 
 logger = getLogger(__name__)
 
@@ -23,18 +24,3 @@ try:
 # Ignore ImportErrors, if setup.py is invoked outside a virtualenv
 except ImportError as e:
     logger.warning(e)
-
-
-def get_prerelease_version(version: str) -> str:
-    """If we're running in a GitHub Action job on the dev branch, get a prerelease semantic version
-    using the current build number. For example: ``1.0.0 -> 1.0.0-dev.123``
-    """
-    if getenv('GITHUB_REF') == 'refs/heads/dev':
-        build_number = getenv('GITHUB_RUN_NUMBER', '0')
-        version = f'{version}.dev{build_number}'
-        logger.info(f'Using pre-release version: {version}')
-    return version
-
-
-# This won't modify the version outside of a GitHub Action
-__version__ = get_prerelease_version(__version__)
