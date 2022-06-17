c6bf99f7a6b5aaebac16b3c430a5b95e57f02633
Jordan Cook <jordan.cook@pioneer.com>
2022-06-16 21:58:15 -0500

    Add optimized delete() and filter() methods

--- requests_cache/backends/__init__.py
+++ requests_cache/backends/__init__.py
@@ -19,9 +19,9 @@ logger = getLogger(__name__)
 
 # Import all backend classes for which dependencies are installed
 try:
-    from .db import DBCache, DBStorage
+    from .db import DbCache, DbDict
 except ImportError as e:
-    DBCache = DBStorage = get_placeholder_class(e)  # type: ignore
+    DbCache = DbDict = get_placeholder_class(e)  # type: ignore
 
 try:
     from .dynamodb import DynamoDbCache, DynamoDbDict
@@ -55,7 +55,7 @@ except ImportError as e:
 
 
 BACKEND_CLASSES = {
-    'db': DBCache,
+    'db': DbCache,
     'dynamodb': DynamoDbCache,
     'filesystem': FileCache,
     'gridfs': GridFSCache,
