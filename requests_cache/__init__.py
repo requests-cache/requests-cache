2dc8c4e95b8591d91577843dd327b8227c72cca0
Jordan Cook <jordan.cook.git@proton.me>
2023-01-13 14:29:26 -0600

    Bump version

--- requests_cache/__init__.py
+++ requests_cache/__init__.py
@@ -2,12 +2,12 @@
 
 # Version is defined in pyproject.toml.
 # It's copied here to make it easier for client code to check the installed version.
-__version__ = '0.9.7'
+__version__ = '0.9.8'
 
 from .backends import *
-from .policy import *
 from .cache_keys import *
 from .models import *
 from .patcher import *
+from .policy import *
 from .serializers import *
 from .session import *
