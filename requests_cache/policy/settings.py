5a03b3a10e32e99a94b251e8af3523867dd3c2ff
Jordan Cook <jordan.cook.git@proton.me>
2023-10-12 11:29:38 -0500

    Fix default value for CacheSettings.always_revalidate (only affects str representation)

--- requests_cache/policy/settings.py
+++ requests_cache/policy/settings.py
@@ -25,7 +25,7 @@ class CacheSettings(RichMixin):
 
     allowable_codes: Iterable[int] = field(default=DEFAULT_STATUS_CODES)
     allowable_methods: Iterable[str] = field(default=DEFAULT_METHODS)
-    always_revalidate: bool = field(default=None)
+    always_revalidate: bool = field(default=False)
     cache_control: bool = field(default=False)
     disabled: bool = field(default=False)
     expire_after: ExpirationTime = field(default=None)
